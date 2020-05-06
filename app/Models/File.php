851c0c0022b1053a15de07a9a1912feb642df984
polokh ivan <ivan.polokh.98@gmail.com>
2020-05-06 09:38:09 +0300

    avatar save

--- app/Models/File.php
+++ app/Models/File.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    public function saveAvatar($request)
+    {
+        if ($request->hasFile('avatar')) {
+            $file = $request->file('avatar');
+
+            return $data['path'] = $file->store('avatars','public');
+        }
+    }
 }
