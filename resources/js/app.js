c37e4198d262038bbc9f93593e6535a4fbf70eec
no4ch <vk202164356@gmail.com>
2020-05-08 20:03:35 +0300

    last fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,7 @@ import VeeValidate from './validation';
 
 Vue.component('left-nav', require('./components/left-nav/LeftNav.vue').default);
 Vue.component('tools', require('./components/tools/Tools.vue').default);
+Vue.component('contact-list', require('./components/pages/contact-list/ContactList').default);
 
 const app = new Vue({
     el: '#app',
--- resources/js/store/index.js
+++ resources/js/store/index.js
@@ -4,6 +4,8 @@ import axios from "axios";
 import groups from "./modules/groups/groups";
 import contacts from "./modules/contacts/contacts";
 import contactForms from "./modules/contacts/contact-forms";
+
+import contactList from "./modules/contacts/contact-list"
 Vue.use(Vuex)
 
 export default new Vuex.Store({
@@ -22,6 +24,7 @@ export default new Vuex.Store({
     modules: {
         groups,
         contacts,
-        contactForms
+        contactForms,
+        contactList
     }
 })
