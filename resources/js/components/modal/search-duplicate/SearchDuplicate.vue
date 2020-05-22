9c74378a4427ca16edd61213cd26e02f7f637573
polokh ivan <ivan.polokh.98@gmail.com>
2020-05-22 15:49:39 +0300

    fix store

--- resources/js/components/modal/search-duplicate/SearchDuplicate.vue
+++ resources/js/components/modal/search-duplicate/SearchDuplicate.vue
@@ -14,12 +14,12 @@
                                             <label class="duplicate font-14px" for="test1"></label>
                                         </span>
                                             <img class="contactPhoto mr-2" :src="getDuplicated[0].avatar?'/storage/'+getDuplicated[0].avatar:'/storage/avatars/default.png'"  alt="фото">
-                                            {{getDuplicated[0].fio}}
+                                            {{getDuplicated[0].fio|truncate(30, '...')}}
                                         </div>
                                         <div class="col-4">
-                                            {{getDuplicated[0].email}}
+                                            {{getDuplicated[0].email|truncate(25, '...')}}
                                         </div>
-                                        <div class="col-3=">
+                                        <div class="col-3">
                                             {{getDuplicated[0].number}}
                                         </div>
                                     </div>
@@ -30,23 +30,21 @@
                                             <label class="duplicate font-14px" for="test2"></label>
                                         </span>
                                             <img class="contactPhoto mr-2" :src="getDuplicated[1].avatar?'/storage/'+getDuplicated[1].avatar:'/storage/avatars/default.png'"  alt="фото">
-                                            {{getDuplicated[1].fio}}
+                                            <span>{{getDuplicated[1].fio|truncate(21, '...')}}</span>
                                         </div>
-                                        <div class="d-none d-lg-flex col-lg-4">
-                                            {{getDuplicated[1].email}}
+                                        <div class="col-4">
+                                            <span>{{getDuplicated[1].email|truncate(25, '...')}}</span>
                                         </div>
-                                        <div class="col-5 col-md-4 col-lg-3 col-xl-3=">
-                                            {{getDuplicated[1].number}}
+                                        <div class="col-3">
+                                            <span>{{getDuplicated[1].number}}</span>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                         <div class="modal-footer">
-                            <div class="d-flex justify-content-end w-100">
                                 <button @click="confirmed" type="button" class="btn ok px-3 py-3 text-uppercase font-12px" :disabled="mainContact==''">обьеденить</button>
                                 <button type="button" class="btn px-3 py-3 ml-4 modalText bthCancel font-12px text-uppercase" data-dismiss="modal">закрыть</button>
-                            </div>
                         </div>
                     </template>
                     <template v-else>
@@ -95,6 +93,15 @@
 
                return this.$store.getters.getDuplicatedContacts;
            }
+        },
+        filters: {
+            truncate: function (text, length, suffix) {
+                if (text.length > length) {
+                    return text.substring(0, length) + suffix;
+                } else {
+                    return text;
+                }
+            },
         }
     }
 </script>
